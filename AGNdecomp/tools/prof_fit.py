1f64e9b81eaf629fdf8594fdba3427b9ecfc316e
hjibarram <hjibarram@gmail.com>
2025-07-12 15:08:20 -0600

    Update prof_fit.py

--- AGNdecomp/tools/prof_fit.py
+++ AGNdecomp/tools/prof_fit.py
@@ -212,6 +212,7 @@ def prof_ana(cube,cubeE,hdr,sig=2,prior_config='priors_prop.yml',mod_ind=0,verbo
             if pi_x == 0 and pi_y == 0:
                 fcenter=False    
             if moffat:
+                print('A')
                 dx_m1,dy_m1,ds_m1,db_m1,psf1,Ft,FtF,Io_m,bn_m,Re_m,ns_m,At0,e0_m,th0_m=evaluate_2dPSF(map1,map1e,name=name+spt,Namevalues=Namevalues,Inpvalues=Inpvalues,Infvalues=Infvalues,Supvalues=Supvalues,model=False,ring=ring,trip=trip,sig=sig,fcenter=fcenter,beta=beta,ellip=ellip,re_int=re_int,singlepsf=singlepsf,moffat=moffat,mc=mc,ncpu=ncpu,db_m=bt,psf_coef=psf_coef,pi_x=pi_x,pi_y=pi_y,bs_c=bs_c,Re_c=Re_c,ns_c=ns_c,psft=psft,ro_i=ro_i,ds_i=ds_i,Lt_c=Lt_c,e_m=et_c,tht_m=th_c) 
             else:
                 dx_m1,dy_m1,ds_m1,psf1,Ft,FtF=evaluate_2dPSF(map1,map1e,name=name+spt,model=False,sig=sig,mc=mc,ncpu=ncpu)
