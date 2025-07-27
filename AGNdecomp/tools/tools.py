bf2175601274d629abdabcaba66501a2113f3d5a
hjibarram <hjibarram@gmail.com>
2025-07-26 22:16:10 -0600

    updates

--- AGNdecomp/tools/tools.py
+++ AGNdecomp/tools/tools.py
@@ -305,6 +305,43 @@ def step_vect_Mean(flux,sp=20,pst=True,mask_val=[]):
             flux_t[i]=np.nanmean(flux_0[i0:i1])
     return flux_t    
 
+def get_error_cube(cubeI,sp=20,pst=True,sigma=10):
+    nz,nx,ny=cubeI.shape
+    cubeE= np.copy(cubeI)
+    for i in range(0, nx):
+        for j in range(0, ny):
+            flux=cubeI[:,i,j]
+            flux_t=step_vect2(flux,sp=sp,pst=pst,sigma=sigma)
+            cubeE[:,i,j]=flux_t
+    return cubeE
+
+def step_vect2(fluxi,sp=20,pst=True,sigma=10):
+    flux_sm=conv(fluxi,ke=sigma)
+    flux=fluxi-flux_sm
+    nz=len(flux)
+    flux_t=np.copy(flux)
+    for i in range(0, nz):
+        i0=int(i-sp/2.0)
+        i1=int(i+sp/2.0)
+        if i1 > nz:
+            i1=nz
+        if i0 > nz:
+            i0=int(nz-sp)
+        if i0 < 0:
+            i0=0
+        if i1 < 0:
+            i1=sp   
+        if pst:
+            lts=np.nanpercentile(flux[i0:i1],78)
+            lt0=np.nanpercentile(flux[i0:i1],50)
+            lti=np.nanpercentile(flux[i0:i1],22)
+            val=(np.abs(lts-lt0)+np.abs(lti-lt0))/2.0
+            flux_t[i]=val#mean
+        else:
+            flux_t[i]=np.nanstd(flux[i0:i1])
+    return flux_t
+
+
 def step_vect(flux,sp=20,pst=True):
     nz=len(flux)
     flux_t=np.copy(flux)
