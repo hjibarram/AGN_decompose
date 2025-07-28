62a4d333b7267338e09f56de649e16819a7dcd61
hjibarram <hjibarram@gmail.com>
2025-07-28 11:57:53 -0600

    Update models.py

--- AGNdecomp/tools/models.py
+++ AGNdecomp/tools/models.py
@@ -116,7 +116,10 @@ def get_model(dir_o='./',dir_cube='./',vt='',hdri0=0,hdri1=1,hdri2=2,dir_cube_m=
     outf1='Model_'+basename.replace('.fits','').replace('.gz','').replace('NAME',name+vt)
     outf3='Residual_'+basename.replace('.fits','').replace('.gz','').replace('NAME',name+vt)
     [cube0, hdr0]=fits.getdata(dir_cube+cube_file, hdri0, header=True)
-    [cube1, hdr1]=fits.getdata(dir_cube+cube_file, hdri1, header=True)
+    try:
+        [cube1, hdr1]=fits.getdata(dir_cube+cube_file, hdri1, header=True)
+    except:
+        [cube1, hdr1]=[cube0, hdr0]
     try:
         [cube2, hdr2]=fits.getdata(dir_cube+cube_file, hdri2, header=True)
     except:
