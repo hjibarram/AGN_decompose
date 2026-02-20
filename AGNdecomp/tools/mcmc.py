afdcf64945d9fb89bf3f9453d1beff40f556cb3e
hjibarram <hjibarram@gmail.com>
2026-02-20 09:08:27 +0100

    Update mcmc.py

--- AGNdecomp/tools/mcmc.py
+++ AGNdecomp/tools/mcmc.py
@@ -123,8 +123,8 @@ def evaluate_2dPSF(pf_map,pf_mapE,name='test',Model_name='moffat',Usermods=['','
             spec_hst=spec_agn*0
         tolpl.plot_models_maps(pf_map,spec_agn,spec_hst,samples,name=name,path_out=path_out,savefig=savefig,Labelvalues=Labelvalues,logP=logP,stl=stl,smoth=smoth,sig=sigm,ofsval=ofsval,get_modelhost=get_modelhost)
     try:
-    pars_max['xo']=pars_max['xo']+min_in[1]
-    pars_max['yo']=pars_max['yo']+min_in[0]
+        pars_max['xo']=pars_max['xo']+min_in[1]
+        pars_max['yo']=pars_max['yo']+min_in[0]
     except:
         pass
     return pars_max,psf,ft_num,ft_fit
